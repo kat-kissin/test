8fac326593c20afe762ac750e9f6b6f15aac0c08
kat <germanova@hotmail.fr>
2020-09-09 13:18:29 +0200

    WIP

--- src/main/java/com/example/test/TestApplication.java
+++ src/main/java/com/example/test/TestApplication.java
@@ -9,7 +9,6 @@ public class TestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TestApplication.class, args);
 		System.out.println("Hello world");
-		System.out.println("Hello world number 2");
 	}
 
 }
